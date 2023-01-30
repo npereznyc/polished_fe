fa9cdf6558ca9e2cc36b748cc9753df77135cea3
npereznyc <natalieperez84@gmail.com>
2023-01-30 12:24:38 -0500

    Reading from backend and rendering in browser

---
+++ src/pages/PolishList.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function PolishList() {
+	const [polishList, setPolishList] = useState([])
+
+	useEffect(() => {
+		axios
+			.get("/api/polished/")
+			.then((res) => {
+				console.log(res.data)
+				setPolishList(res.data)
+			})
+			.catch((err) => console.log(err))
+	}, [])
+
+
+
+	return (
+		<div>
+			<div>
+				{polishList.map((e, index) => {
+					return (
+						<div key={index}>
+							<p> {e.name}</p>
+							<p> {e.brand}</p>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
+}
+
+export default PolishList;
